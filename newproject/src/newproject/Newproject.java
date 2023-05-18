9230d11c0dbe3373686125b02031313c0465b72d
harsh <vardeh@sheridancollege.ca>
2023-05-18 15:10:55 -0400

    mt first commit

---
+++ newproject/src/newproject/Newproject.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package newproject;
+
+/**
+ *
+ * @author hp
+ */
+public class Newproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=20,b=50,c=10;
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(c, b));
+    }
+    
+}

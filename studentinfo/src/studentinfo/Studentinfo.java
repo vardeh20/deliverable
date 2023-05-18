48df88d8d47ec833693a6b3740dd2416bc26875e
harsh <vardeh@sheridancollege.ca>
2023-05-18 15:41:00 -0400

    mt first commit

---
+++ studentinfo/src/studentinfo/Studentinfo.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentinfo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class Studentinfo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+        Scanner sc= new Scanner(System.in);
+       Student[] stu= new Studentinfo[5];
+        Studentinfo[0] = new Studentinfo("harsh");
+        Studentinfo[1]= new Studentinfo("shubh");
+        Studentinfo[2] = new Studentinfo("dhruve");
+        Studentinfo[3]= new Studentinfo("meet");
+        Studentinfo[4] = new Studentinfo("vipul");
+        
+       
+        
+    }
+    
+    
+}
+

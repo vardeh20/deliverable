4a94e0d55699d4d0b732f2e9f4fb96a036c44f18
harsh <vardeh@sheridancollege.ca>
2023-08-14 22:32:38 -0400

    delivrable 3

---
+++ delivrable3/src/delivrable3/UnoCard.java
@@ -0,0 +1,33 @@
+package delivrable3;
+public class UnoCard {
+    private String color;
+    private String value;
+
+    public UnoCard(String color, String value) {
+        this.color = color;
+        this.value = value;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return color + " " + value;
+    }
+
+
+
+    public static void main(String[] args) {
+        UnoCard card = new UnoCard("Red", "5");
+        System.out.println("Color: " + card.getColor());
+        System.out.println("Value: " + card.getValue());
+        System.out.println("Card: " + card);
+    }
+}
+
---
+++ delivrable3/src/delivrable3/UnoDeck.java
@@ -0,0 +1,55 @@
+package delivrable3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UnoDeck {
+    private List<UnoCard> cards;
+
+    public UnoDeck() {
+        cards = new ArrayList<>();
+        initializeDeck();
+        shuffle();
+    }
+
+    private void initializeDeck() {
+        String[] colors = {"Red", "Blue", "Green", "Yellow"};
+        String[] values = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Skip", "Reverse", "Draw Two"};
+
+        for (String color : colors) {
+            for (String value : values) {
+                cards.add(new UnoCard(color, value));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public UnoCard drawCard() {
+        if (!cards.isEmpty()) {
+            return cards.remove(cards.size() - 1);
+        }
+        return null;
+    }
+
+    public int size() {
+        return cards.size();
+    }
+
+    public static void main(String[] args) {
+        UnoDeck deck = new UnoDeck();
+        System.out.println("Deck size: " + deck.size());
+
+        UnoCard drawnCard = deck.drawCard();
+        if (drawnCard != null) {
+            System.out.println("Drawn card: " + drawnCard);
+            System.out.println("Deck size after drawing: " + deck.size());
+        } else {
+            System.out.println("Deck is empty.");
+        }
+    }
+}
+
